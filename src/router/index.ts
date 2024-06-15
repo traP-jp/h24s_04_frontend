52a8de7e3ee48dcc6a88eb3e4596eabd489f1870
Hyde <orange.people.345@gmail.com>
2024-06-15 10:16:02 +0900

    ListView:スライド(仮)の表示、レイアウト

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue')
+      component: () => import('../views/ListView.vue')
     }
+
   ]
 })
 
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,7 +1,44 @@
 <script setup lang="ts">
+import { ref, computed} from 'vue'
 
+interface Data{
+    title: string
+    description: string
+    genre: string
+}
+
+const datas = ref<Data[]>([
+    {   
+        title: 'slide1',
+        description: 'test1',
+        genre: 'hackathon'
+    },
+    {
+        title:'slide2',
+        description: 'test2',
+        genre: 'hackathon'
+    }
+])
 </script>
 
 <template>
-
+    <div>
+        <h1>スライド一覧</h1>
+    </div>
+    <ul class='container'>
+        <li v-for="data in datas" :key="data.title">
+            <div>タイトル：{{  data.title }}</div>
+            <div>詳細：{{  data.description }}</div>
+            <div>ジャンル：{{  data.genre }}</div>
+        </li>
+    </ul>
 </template>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        padding: 16px 16px;
+    }
+</style>
