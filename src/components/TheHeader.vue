0eae8e3f88f6eb5f91d2ed0611c8519e452b142a
koki <kn.tokyotech21295@gmail.com>
2024-06-16 12:37:15 +0900

    Header color

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,15 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import {computed} from 'vue'
+
+const route = useRoute()
+const url = computed(()=>route.fullPath)
+</script>
+
 <template>
   <header :class="$style.header">
-    <router-link to="/">スライド一覧</router-link>
-    <router-link to="/upload">スライドの登録</router-link>
+    <router-link to="/" :aria-current="url === '/' ? 'page' : undefined">スライド一覧</router-link>
+    <router-link to="/upload" :aria-current="url === '/upload' ? 'page' : undefined">スライドの登録</router-link>
   </header>
 </template>
 
@@ -20,6 +28,9 @@
     border-radius: 8px;
 
     &:hover {
+      background-color: #f0f0f0;
+    }
+    &[aria-current = 'page']{
       background-color: #f0f0f0;
     }
   }
